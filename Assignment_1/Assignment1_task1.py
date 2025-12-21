18be9695ed8a41406061910ac1d563666307c255
Pragathi Kota <69187691+pragathi528@users.noreply.github.com>
2025-12-21 19:06:58 +0530

    Add files via upload

---
+++ Assignment_1/Assignment1_task1.py
@@ -0,0 +1,11 @@
+#Task 1: Perform Basic Mathematical Operations
+
+#1.  Takes two numbers as input from the user.
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+#2.  Performs the basic mathematical operations on these two numbers:
+print(f"Addition: {number1 + number2}")
+print(f"Subtraction: {number1 - number2}")
+print(f"Multiplication: {number1 * number2}")
+print(f"Division: {number1 / number2}")
---
+++ Assignment_1/Assignment1_task2.py
@@ -0,0 +1,9 @@
+#1.  Takes a user's first name and last name as input.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#2.  Concatenates the first name and last name into a full name.
+full_name = first_name + " " + last_name
+
+#3.  Prints a personalized greeting message using the full name.
+print(f"Hello, {full_name}! Welcome to Python Program.")
